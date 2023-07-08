bd647cbf0866c5898a95ce00d03235caef926811
kiranaradhya20 <80210004+kiranaradhya20@users.noreply.github.com>
2023-07-08 18:52:51 +0530

    Update predictionDataValidation.py

--- Prediction_Raw_Data_Validation/predictionDataValidation.py
+++ Prediction_Raw_Data_Validation/predictionDataValidation.py
@@ -16,10 +16,6 @@ class Prediction_Data_validation:
     """
                This class shall be used for handling all the validation done on the Raw Prediction Data!!.
 
-               Written By: iNeuron Intelligence
-               Version: 1.0
-               Revisions: None
-
                """
 
     def __init__(self,path):
@@ -34,10 +30,6 @@ class Prediction_Data_validation:
                                 Description: This method extracts all the relevant information from the pre-defined "Schema" file.
                                 Output: LengthOfDateStampInFile, LengthOfTimeStampInFile, column_names, Number of Columns
                                 On Failure: Raise ValueError,KeyError,Exception
-
-                                 Written By: iNeuron Intelligence
-                                Version: 1.0
-                                Revisions: None
 
                                         """
         try:
@@ -88,10 +80,6 @@ class Prediction_Data_validation:
                                       Output: Regex pattern
                                       On Failure: None
 
-                                       Written By: iNeuron Intelligence
-                                      Version: 1.0
-                                      Revisions: None
-
                                               """
         regex = "['cement_strength']+['\_'']+[\d_]+[\d]+\.csv"
         return regex
@@ -105,10 +93,6 @@ class Prediction_Data_validation:
 
                                         Output: None
                                         On Failure: OSError
-
-                                         Written By: iNeuron Intelligence
-                                        Version: 1.0
-                                        Revisions: None
 
                                                 """
         try:
@@ -134,10 +118,6 @@ class Prediction_Data_validation:
                                             Output: None
                                             On Failure: OSError
 
-                                             Written By: iNeuron Intelligence
-                                            Version: 1.0
-                                            Revisions: None
-
                                                     """
         try:
             path = 'Prediction_Raw_Files_Validated/'
@@ -161,10 +141,6 @@ class Prediction_Data_validation:
                                             Description: This method deletes the directory made to store the bad Data.
                                             Output: None
                                             On Failure: OSError
-
-                                             Written By: iNeuron Intelligence
-                                            Version: 1.0
-                                            Revisions: None
 
                                                     """
 
@@ -191,10 +167,6 @@ class Prediction_Data_validation:
                                                           files to send them back to the client for invalid data issue.
                                             Output: None
                                             On Failure: OSError
-
-                                             Written By: iNeuron Intelligence
-                                            Version: 1.0
-                                            Revisions: None
 
                                                     """
         now = datetime.now()
@@ -236,10 +208,6 @@ class Prediction_Data_validation:
                          to Bad Raw Data folder else in Good raw data.
             Output: None
             On Failure: Exception
-
-             Written By: iNeuron Intelligence
-            Version: 1.0
-            Revisions: None
 
         """
         # delete the directories for good and bad data in case last run was unsuccessful and folders were not deleted.
@@ -290,10 +258,6 @@ class Prediction_Data_validation:
                     Output: None
                     On Failure: Exception
 
-                     Written By: iNeuron Intelligence
-                    Version: 1.0
-                    Revisions: None
-
              """
         try:
             f = open("Prediction_Logs/columnValidationLog.txt", 'a+')
@@ -334,10 +298,6 @@ class Prediction_Data_validation:
                                                SUch files are moved to bad raw data.
                                   Output: None
                                   On Failure: Exception
-
-                                   Written By: iNeuron Intelligence
-                                  Version: 1.0
-                                  Revisions: None
 
                               """
         try:
